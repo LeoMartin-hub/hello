06c11efa26e95a6842d7fbbf233732e9984b47b3
ajrbo <ajrbo@MSI-Laptop>
2023-08-27 12:26:58 -0500

    Initial commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,8 @@
 
 public class Hello {
 	public static void main(String args[]) {
-	System.out.print("Leo Martin");
+	System.out.print("Leo Martin ");
+	System.out.print("Alex Robinson. ");
+	System.out.print("I am a computer Engineer Major");
 	}
 }
